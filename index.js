6a557905c14f42a327be50dcaa97c2479cd63963
thekonly <joaopedro_m@icloud.com>
2021-09-14 18:05:03 -0400

    Completed.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(function(word) {
+        return word.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(word) {
+        return word[0] === string[0];
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(word => word.name === string);
+}
